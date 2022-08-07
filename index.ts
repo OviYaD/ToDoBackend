917730d80b34c52650ebd23863d3d1733bb4465c
oviyad <oviyad.19cse@kongu.edu>
2022-08-07 20:39:40 +0530

    implemented login and register

--- index.ts
+++ index.ts
@@ -1,13 +1,26 @@
 import express from "express";
+import cors from "cors";
 import authRoute from "./routes/auth";
+import dotenv from "dotenv";
+import { connectToDatabase } from "./services/database";
 
-const app = express();
+dotenv.config();
+connectToDatabase()
+  .then(() => {
+    const app = express();
 
-app.use("/auth", authRoute);
+    app.use(cors());
+    app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.listen(5000, () => {
-  console.log("Example app listening on port 3000!");
-});
+    app.use("/auth", authRoute);
+
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
+    app.listen(5000, () => {
+      console.log("Example app listening on port 3000!");
+    });
+  })
+  .catch((err) => {
+    console.log("error:" + err);
+  });
--- routes/auth.ts
+++ routes/auth.ts
@@ -1,8 +1,41 @@
 import { Router } from "express";
+import User from "../models/User";
+import { collections } from "../services/database";
+import bcrypt from "bcrypt";
 
 const router = Router();
-router.get("/login", (req, res) => {
-  res.send("hi!");
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = req.body as User;
+    console.log(user);
+    // user.password = await bcrypt.hash(user.password, 10);
+    const userFromDB = await collections.users?.findOne({ email: user.email });
+    console.log(user.password, userFromDB?.password);
+    // const result = await collections.users?.findOne(user);
+    const isValid = await bcrypt.compare(user.password, userFromDB?.password);
+    isValid
+      ? res.status(201).send("authenticated")
+      : res.status(500).send("Failed to login.");
+  } catch (error: any) {
+    console.error(error);
+    res.status(400).send(error.message);
+  }
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const user = req.body as User;
+    user.password = await bcrypt.hash(user.password, 10);
+    const result = await collections.users?.insertOne(user);
+
+    result
+      ? res.status(201).send(result)
+      : res.status(500).send("Failed to create a user.");
+  } catch (error: any) {
+    console.error(error);
+    res.status(400).send(error.message);
+  }
 });
 
 export default router;
